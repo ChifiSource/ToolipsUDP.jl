978b773b3b7902e51b7a942e0d08ec169191c2ab
emmy <52672675+emmaccode@users.noreply.github.com>
2023-12-18 00:22:34 -0700

    surprise! I'm back to work on this :)

--- src/ToolipsUDP.jl
+++ src/ToolipsUDP.jl
@@ -1,72 +1,55 @@
-"""
-Created in February, 2022 by
-[chifi - an open source software dynasty.](https://github.com/orgs/ChifiSource)
-by team
-[toolips](https://github.com/orgs/ChifiSource/teams/toolips)
-This software is MIT-licensed.
-### ToolipsUDP
-ToolipsUDP provides a simple `UDPServer` which works with toolips infastructure.
-##### Module Composition
-- [**Toolips**](https://github.com/ChifiSource/Toolips.jl)
-"""
 module ToolipsUDP
-using Sockets
-using Sockets: UDPSocket, recv
-import Sockets: send
 using Toolips
-import Toolips: ToolipsServer, AbstractConnection
+using Toolips.Sockets
+import Toolips.Sockets: send, write!
+using Toolips: ServerExtension, ToolipsServer, AbstractConnection
+import Base: show, read
 
-"""
-### UDPServer <: Toolips.ToolipsServer
-- ip::String
-- port::Int64
-- extensions::Vector{ServerExtension}
-- start::Function
-- server::Any\n
-The UDPServer is not a traditional webserver; it is focused
-on serving clients a lot faster and is typically used for bigger data transfer
-    scenarios, such as a video game. That being said, that is exactly the kind
-    of thing that this module is for.
-##### example
-```
-using Toolips
-using ToolipsUDP
-myserver = UDPServer("127.0.0.1", 8000) do c::Connection
-myserver.start()
-```
-------------------
-##### constructors
-- UDPServer(f::Function)
-"""
+mutable struct UDPConnection <: AbstractConnection
+    data::String
+end
+
 mutable struct UDPServer <: ToolipsServer
-    ip::String
-    PORT::Int64
-    extensions::Vector{ServerExtension}
+    host::String
+    port::Int64
+    server::Sockets.UDPSocket
     start::Function
-    server::Any
-    f::Function
-    function UDPServer(f::Function, ip::String = "128.0.0.1", port::Int64 = 8000;
-        extensions = Vector{ServerExtension}([Logger()]))
-        server::Any = []
+    function UDPServer(f::Function, host::String = "127.0.0.1", port::Integer = 2000)
+        server = UDPSocket()
         start() = begin
-            task = @async begin
-                sock::UDPSocket = UDPSocket()
-                bind(sock, Sockets.IPv4(ip), port)
-                while true
-                    mydata::String = String(read(sock))
-                    f(mydata)
-                end
-                close(sock)
+            bind(server, parse(IPv4, host), port)
+            @async while server.status == 3
+                con = UDPConnection(String(recv(server)))
+                f(con)
             end
-            push!(server, task)
         end
-        new(ip, port, extensions, start, server, f)::UDPServer
+        new(host, port, server, start)
     end
 end
 
-function send(s::String, ip::String = "127.0.0.1", port::Int64 = 8000)
-     soc = UDPSocket()
-     send(soc, Sockets.IPv4(ip), port, s)
+function show(io::IO, ts::UDPServer)
+    status::String = string(ts.server.status)
+    active::String = "inactive"
+    if status == "3" || "4"
+        active = active
+    end
+    print("""$(typeof(ts))
+        UDP server: $(ts.host):$(ts.port)
+        status: $status $active
+        """)
 end
-export send, UDPServer
-end # module
+
+function send(to::String = "127.0.0.1", port::Int64 = 2000, data::String; from::Int64 = port - 5)
+    sock = UDPSocket()
+    bind(sock, ip"127.0.0.1", from)
+    send(sock, parse(IPv4, to), port, data)
+    sock
+end
+
+function send(c::UDPConnection, port::Int64 = 2000, data::String)
+    sock = UDPSocket()
+    bind(sock, ip"127.0.0.1", from)
+    send(sock, parse(IPv4, from), port, data)
+end
+
+end # module ToolipsUDP
