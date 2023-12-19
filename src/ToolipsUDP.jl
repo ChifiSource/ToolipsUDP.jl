e8263809352c8014ec592f44f55b214468e7fe7f
emmy <52672675+emmaccode@users.noreply.github.com>
2023-12-18 22:07:50 -0700

    honestly -- getting kind of close to finishing, already :)

--- src/ToolipsUDP.jl
+++ src/ToolipsUDP.jl
@@ -1,21 +1,22 @@
 module ToolipsUDP
 using Toolips.Sockets
+using Toolips.Pkg: activate, add
 import Toolips.Sockets: send
-import Toolips: ServerExtension, ToolipsServer, AbstractConnection, getip
+import Toolips: ServerExtension, ToolipsServer, AbstractConnection, getip, write!, new_app
 import Base: show, read
 
 mutable struct UDPConnection <: AbstractConnection
-    data::String
     ip::String
+    port::Int64
+    data::String
     client::Dict{Symbol, String}
     server::Sockets.UDPSocket
     function UDPConnection(client::Dict{Symbol, Any}, server::Sockets.UDPSocket)
-     #   ip_data = recvfrom(server)
-     #   ip, data = String(ip_data[1]), String(ip_data[2])
-     ip, rawdata = recvfrom(server)
-     data = String(rawdata)
-     ip = string(ip)
-        new(data, ip, client, server)::UDPConnection
+        ip, rawdata = recvfrom(server)
+        data = String(rawdata)
+        port = Int64(ip.port)
+        ip = string(ip.host)
+        new(ip, port, data, client, server)::UDPConnection
     end
 end
 
@@ -31,10 +32,10 @@ mutable struct UDPServer <: ToolipsServer
     start::Function
     function UDPServer(f::Function, host::String = "127.0.0.1", port::Integer = 2000)
         server = UDPSocket()
-        ms = methods(serve)
-        exlist = [m.sig.parameters[3] for m in ms]
-        data::Dict{Symbol, Any} = Dict{Symbol, Any}()
         start() = begin
+            ms = methods(serve)
+            exlist = [m.sig.parameters[3] for m in ms]
+            data::Dict{Symbol, Any} = Dict{Symbol, Any}()
             bind(server, parse(IPv4, host), port)
             Threads.@spawn while server.status == 3
                 con::UDPConnection = UDPConnection(data, server)
@@ -50,11 +51,46 @@ mutable struct UDPServer <: ToolipsServer
                 end
             end
         end
-        new(host, port, server, start)
+        new(host, port, server, start)::UDPServer
+    end
+    UDPServer(host::String, port::Integer) = UDPServer(c::UDPConnection -> nothing, host, port)::UDPServer
+end
+
+function new_app(name::String, T::Type{UDPServer})
+    Toolips.new_app(name)
+    activate(name)
+    add("ToolipsUDP")
+    open("$name/src/$name.jl", "w") do o::IO
+        write(o, 
+        """module $name
+        using ToolipsUDP
+
+        function start(ip::String = "127.0.0.1", port::Int64 = 2000)
+            myserver = UDPServer() do c::UDPConnection
+                println(c.data)
+                println(c.ip)
+                println(c.port)
+            end
+            myserver.start()
+            myserver
+        end
+
+        function send_to_my_server(data::String)
+            server2 = UDPServer("127.0.0.1", 2005)
+            server2.start()
+            ToolipsUDP.send(server2, "test", myserver.host, myserver.port)
+        end
+        ==#
+        """)
     end
 end
 
 function serve(c::UDPConnection, ext::UDPExtension{<:Any})
+
+end
+
+function start(c::UDPConnection, ext::UDPExtension{<:Any})
+
 
 end
 
